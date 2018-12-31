2cbdce75cb1b0063514698f0707daafe88e58710
mmm <mmm@qq.com>
2018-12-31 15:35:21 +0800

    SpringCloudLearning

---
+++ springcloudzuulgatway/src/main/java/com/atguigu/springcloud/ZuulMainApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulMainApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulMainApplication.class,args);
+    }
+}
