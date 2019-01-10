67dd395ed398293c733519418c02b05fbde4ec24
mmm <mmm@qq.com>
2019-01-10 13:07:14 +0800

    SpringCloudLearning

--- springcloudconsumerdeptfeign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
+++ springcloudconsumerdeptfeign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 @EnableFeignClients(basePackages= {"com.atguigu.springcloud"})
 @ComponentScan("com.atguigu.springcloud")
+@EnableCircuitBreaker
 public class DeptConsumer80_Feign_App
 {
 	public static void main(String[] args)
